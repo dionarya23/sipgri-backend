cf878700ed5083ab0288e171ddb5a2027b91f8ea
Dion Arya Pamungkas <dionarya.p@gmail.com>
2021-04-25 11:22:03 +0700

    add cors

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -13,6 +13,16 @@ import (
 
 func AuthMiddleware(authService auth.Service, guruService guru.Service, typePegawai []string) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
 		authHeader := c.GetHeader("Authorization")
 
 		if !strings.Contains(authHeader, "Bearer") {
