99eb6f18ad42f610220eef90b3d2bb3c53c95881
Dion Arya Pamungkas <dionarya.p@gmail.com>
2021-04-26 14:10:11 +0700

    try fix cors issues

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -11,18 +11,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func AuthMiddleware(authService auth.Service, guruService guru.Service, typePegawai []string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-
-		// c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		// c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		// c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		// c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
-
-		// if c.Request.Method == "OPTIONS" {
-		// 	c.AbortWithStatus(204)
-		// 	return
-		// }
 
 		authHeader := c.GetHeader("Authorization")
 
